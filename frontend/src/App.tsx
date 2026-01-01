9695a5f8c59da24126d5d331ceb7a2763478708a
odeddidi <odeddidi@gmail.com>
2026-01-01 18:21:05 +0200

    fix navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,28 @@
-import { BrowserRouter, Routes, Route , Navigate} from "react-router-dom";
-import { useState , useEffect} from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import SinglePlayer from "./pages/SinglePlayer";
 import Navbar from "./components/NavBAR";
-import MultiPlayerLobby from "./pages/MultiplayerLobby";  
+import MultiPlayerLobby from "./pages/MultiplayerLobby";
 import Room from "./pages/Room";
 import Profile from "./pages/Profile";
 import AdminPanel from "./pages/AdminPanel";
 import Hero from "./pages/Hero";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-export default function App() {
-const [isLogged, setIsLogged] = useState(!!localStorage.getItem("token"));
-const hasToken = !!localStorage.getItem("token");
-useEffect(() => {
-    const handler = () => {
-      setIsLogged(!!localStorage.getItem("token"));
-    };
-    window.addEventListener("storage", handler);
-    return () => window.removeEventListener("storage", handler);
-  }, []);
 
-  
+export default function App() {
+  const [isLogged, setIsLogged] = useState(() => !!localStorage.getItem("token"));
 
   return (
     <BrowserRouter>
-
-      {!isLogged ? (
-        <Routes>
-          <Route path="/" element={hasToken ? <Navigate to="/home" /> : <Hero />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
-        </Routes>
-      ) : (
+      {isLogged ? (
         <>
           <Navbar />
           <Routes>
+            {/* typing "/" while logged in -> go home */}
+            <Route path="/" element={<Navigate to="/home" replace />} />
+
             <Route path="/home" element={<Home />} />
             <Route path="/single" element={<SinglePlayer />} />
             <Route path="/multi-player-lobby" element={<MultiPlayerLobby />} />
@@ -43,10 +30,17 @@ useEffect(() => {
             <Route path="/profile" element={<Profile />} />
             <Route path="/admin" element={<AdminPanel />} />
 
+            <Route path="*" element={<Navigate to="/home" replace />} />
           </Routes>
         </>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       )}
-
     </BrowserRouter>
   );
 }
